f38bb712ea1c7e054e23ffe7e2822f11ca68eb41
Ayush Newatia <ayush@hey.com>
2020-11-27 16:15:10 +0000

    Replace sass plugin

--- postcss.config.js
+++ postcss.config.js
@@ -4,7 +4,7 @@ module.exports = {
     require("postcss-url")([
       { filter: 'src/_frontend/assets/*.svg', url: 'inline' }
     ]),
-    require('postcss-node-sass'),
+    require('@csstools/postcss-sass'),
     require('postcss-flexbugs-fixes'),
     require('postcss-preset-env')({
       autoprefixer: {
